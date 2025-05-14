ad4f36f6d4cf3cb35ef2c7db9b578284a3bfe914
all_abdel <abdelslam.allaouat.pro@gmail.com>
2025-05-14 11:23:49 +0200

    form

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
+final class ProductController extends AbstractController
+{
+    #[Route('/product', name: 'app_product')]
+    public function index(): Response
+    {
+        return $this->render('product/index.html.twig', [
+            'controller_name' => 'ProductController',
+        ]);
+    }
+    #[Route('/product/create', name: 'product_create')]
+    public function create(Request $request, EntityManagerInterface $em): Response {
+        $product = new Product();
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($product);
+            $em->flush();
+            return $this->redirectToRoute('product_index');
+        }
+
+        return $this->render('product/create.html.twig', ['form' => $form->createView()]);
+}
+
+}
