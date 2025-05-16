51962511af298ed2c30e7f4b91f8b2474a769b0f
all_abdel <abdelslam.allaouat.pro@gmail.com>
2025-05-16 13:12:45 +0200

    sss

--- src/Form/ClientTypeForm.php
+++ src/Form/ClientTypeForm.php
@@ -8,14 +8,59 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 
 class ClientTypeForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class)
-            ->add('email', EmailType::class)
+            ->add('raisonSociale', TextType::class, [
+                'label' => 'Raison sociale *',
+            ])
+            ->add('adresse1', TextType::class, [
+                'label' => 'Adresse 1 *',
+            ])
+            ->add('adresse2', TextType::class, [
+                'label' => 'Adresse 2',
+                'required' => false,
+            ])
+            ->add('adresse3', TextType::class, [
+                'label' => 'Adresse 3',
+                'required' => false,
+            ])
+            ->add('codePostal', TextType::class, [
+                'label' => 'Code postal *',
+            ])
+            ->add('ville', TextType::class, [
+                'label' => 'Ville *',
+            ])
+            ->add('pays', TextType::class, [
+                'label' => 'Pays *',
+            ])
+            ->add('formeJuridique', TextType::class, [
+                'label' => 'Forme juridique *',
+            ])
+            ->add('activite', TextType::class, [
+                'label' => 'Activité *',
+            ])
+            ->add('siret', TextType::class, [
+                'label' => 'SIRET *',
+            ])
+            ->add('actif', CheckboxType::class, [
+                'label' => 'Actif',
+                'required' => false,
+            ])
+            ->add('nomPrenomRepresentant', TextType::class, [
+                'label' => 'Nom et Prénom représentant légal *',
+            ])
+            ->add('emailRepresentant', EmailType::class, [
+                'label' => 'Email représentant légal *',
+            ])
+            ->add('telephoneRepresentant', TelType::class, [
+                'label' => 'Téléphone représentant légal *',
+            ])
         ;
     }
 
