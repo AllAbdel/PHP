11d62c99c09d4f375a942724c74b548ac7da5054
all_abdel <abdelslam.allaouat.pro@gmail.com>
2025-05-16 09:32:20 +0200

    Finish

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Commande;
+use App\Form\CommandeType;
+use App\Form\CommandeTypeForm;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -15,4 +20,31 @@ final class CommandeController extends AbstractController
             'controller_name' => 'CommandeController',
         ]);
     }
+
+/*************  ✨ Windsurf Command ⭐  *************/
+    /**
+     * Creates a new commande entity.
+     *
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+/*******  e282bc5c-84b1-42f0-b8f6-95231283c1bf  *******/
+    #[Route('/commande/create', name: 'commande_create')]
+    public function create(Request $request, EntityManagerInterface $em): Response {
+    $commande = new Commande();
+    $form = $this->createForm(CommandeTypeForm::class, $commande);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        $commande->setCreatedAt(new \DateTimeImmutable());
+        $em->persist($commande);
+        $em->flush();
+        return $this->redirectToRoute('commande_index');
+    }
+
+    return $this->render('commande/create.html.twig', [
+        'form' => $form->createView()
+    ]);
+}
+
 }
